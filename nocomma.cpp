be803714220adb4a16d970924a9cd8c8614a7d62
Deepak Agarwal <deepakjsagarwal@gmail.com>
2023-03-10 15:16:56 +0530

    fixed major bugs to run the code

---
+++ nocomma.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int first_number; int sum;
+  double a = first_number;
+  int d = a + a;
+  //bool b = true;
+
+//   cout << "Enter two integers: ";
+//   cin >> first_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number;
+
+  if (true)
+  {
+//     cout << "how are you";
+      int my_num = first_number+a;
+  }
+
+  // prints sum
+  cout << first_number << " + "<< " = " << sum;
+
+  return 0;
+}
