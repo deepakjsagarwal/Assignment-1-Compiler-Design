3463ca9dc627407d896f755b9e5d24e09ae425e6
Deepak Agarwal <deepakjsagarwal@gmail.com>
2023-03-12 11:30:58 +0530

    added a correct cpp file for testing

--- correct.cpp
+++ correct.cpp
@@ -1,14 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void print(string s){
+    cout<<"String is: "<<s<<endl;
+}
 
+int add(int a, int b){
+    if(true){
+        int c = a+b;
+        return c;
+    }else{
+        int d = a+b;
+        return d;
+    }
+}
 int main(){
+
     int t = 1;
     cin>>t;
+    int a = 5,b = 4,c = a+b;
+    float f1 = 4.20,f2=7.86;
+    char ch1 = 'a',ch2;
 
-
-    while(t--){
-        
+    // if else checking
+    if(ch1==ch2){
+        /*
+            True if both char match
+        */
+    }else{
+        if(f1+1==f2){
+            // True if condition satisfy
+        }
     }
+
+    // for loop checking
+    int i = 0;
+    for(;i<a+c;i++){
+        b+=-(2*i+(a/2+c));  // Expression checking
+    }
+    //while loop checking
+    while(t--){
+        f1 -= 4*f2+a-c;
+    }
+
+    /* do while checking with string and function calls*/
+    do{
+        string s = "I am string checking code.";
+        print(s);
+    }while(true);
+
     return 0;
 }
