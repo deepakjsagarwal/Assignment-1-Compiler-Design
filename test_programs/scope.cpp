ee4b62cd93712134cedc06d539ba4511fc294035
Deepak Agarwal <deepakjsagarwal@gmail.com>
2023-03-12 18:31:17 +0530

    final code

---
+++ test_programs/scope.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i = 0, j =1;
+    
+    if(i==j){
+       int c = 6;
+    }else{
+       int c = 5;
+    }
+    cout<<c<<endl;
+    return 0;
+}
